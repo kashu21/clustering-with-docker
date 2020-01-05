951dcb573e8e95ba316f55a62c518e6d073ef885
ashu <ashu@debian1.edu>
2020-01-05 11:27:53 +0100

    docker compose working fine

---
+++ webservice/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!\n');
+});
+
+app.listen(PORT);
+console.log(`Running on http://docker:${PORT}`);
